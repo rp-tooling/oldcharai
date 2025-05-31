f9644b3ebd354b109aef35251c4a875405066f6e
usernames122 <88596366+usernames122@users.noreply.github.com>
2025-05-31 19:35:10 +0200

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -1,7 +1,3 @@
-// WARNING: You are about to enter a cursed realm
-alert("If C.AI asks you to leave the page, PLEASE CLICK NO! \nOtherwise you will be DOWNGRADED to NewUI");
-
-// Intercept navigation-
 window.addEventListener("beforeunload", (e) => {
     e.preventDefault();
     e.returnValue = "Do you want to be downgraded to newui (click cancel)";
